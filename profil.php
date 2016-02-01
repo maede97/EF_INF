35aa896c79a6b80d675f06c839439c6cb1c7b30e
maede97 <maede97@hotmail.com>
2016-02-01 22:29:35 +0100

    JQuery now local
    Internet connection nicht nötig

--- profil.php
+++ profil.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
+<script src="scripts/jquery.js"></script>
 <script type="text/javascript">
 	$(function(){
 		$("#header").load("header.php");

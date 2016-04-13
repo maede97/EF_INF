0b3cc413718e7e61c6e75c854ff6d91f20f26d4e
maede97 <maede97@hotmail.com>
2016-04-13 19:46:14 +0200

    Links in About-Page

--- content/about.php
+++ content/about.php
@@ -2,3 +2,12 @@
 <hr />
 <p>Diese Seite entstand im Verlaufe des Ergänzungsfaches Informatik an der Kantonsschule Zürcher Oberland in Wetzikon ZH.</p>
 <p>&copy; Jeremy Roth, Matthias Busenhart; 2016 KZO Wetzikon</p>
+<hr />
+<h2>Links</h2>
+<td>
+	<tr><a href="http://github.com/maede97/EF_INF/" target="_blank">Github</a></tr>
+	<tr><a href="http://kzo.ch" target="_blank">KZO</a></tr>
+	<tr><a href="http://github.com/Nokama14/schooltool" target="_blank">Github App</a></tr>
+	<tr><a href="http://github.com/maede97" target="_blank">Github Matthias Busenhart</a></tr>
+	<tr><a href="http://github.com/Nokama14" target="_blank">Github Jeremy Roth</a></tr>
+</td>
